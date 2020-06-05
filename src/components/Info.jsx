a1ec4d77972fd08125deec7d0e79b3fad0189b56
Mohammed Othman <mohammed.othman7@cix.csi.cuny.edu>
2020-06-05 13:54:10 -0400

    Organized files

---
+++ src/components/Info.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const styles = {
+  textAlign: 'center',
+  color: 'white',
+  border: '3px double rgb(12, 184, 232)',
+  borderWidth: '50%',
+  borderRadius: '15px',
+  borderSpacing: '15px',
+  padding: '15px',
+  margin: 'auto',
+  marginBottom: '15px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '65%',
+};
+
+const Info = (props) => {
+  let display;
+  // eslint-disable-next-line react/prop-types
+  const { zipCode } = props;
+  if (zipCode) {
+    display = zipCode.map((e) => (
+      <>
+        <ol key={uuidv4()} style={styles}>
+          <li key={uuidv4()}>
+            City:
+            {e.city}
+          </li>
+          <li key={uuidv4()}>
+            State:
+            {e.state}
+          </li>
+          <li key={uuidv4()}>
+            Country:
+            {e.country}
+          </li>
+        </ol>
+      </>
+    ));
+  } else {
+    display = <></>;
+  }
+  return (
+    <div>
+      <div>{display}</div>
+    </div>
+  );
+};
+
+export default Info;
